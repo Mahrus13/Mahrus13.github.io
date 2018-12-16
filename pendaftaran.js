ebb5349a08a5d88f102247159cb8fe41e6fbb0fa
Mahrus13 <37583235+Mahrus13@users.noreply.github.com>
2018-12-16 17:49:53 +0700

    Add files via upload

---
+++ pendaftaran.js
@@ -0,0 +1,39 @@
+function proses()
+{
+   var input = document.getElementById('nama');
+   var hasil = document.getElementById('k');
+   hasil.innerHTML = input.value;
+
+   var input = document.getElementById('nim');
+   var hasil1 = document.getElementById('s');
+   hasil1.innerHTML = input.value;
+   
+   var input = document.getElementById('jur');
+   var hasil2 = document.getElementById('J');              
+   hasil2.innerHTML = input.value;
+   var input = document.getElementById('email');
+   var hasil3 = document.getElementById('A');              
+   hasil3.innerHTML = input.value;
+   var input = document.getElementById('hp');
+   var hasil4 = document.getElementById('H');              
+   hasil4.innerHTML = input.value;
+   var input = document.getElementById('alamat');
+   var hasil5 = document.getElementById('F');              
+   hasil5.innerHTML = input.value;
+   var input = document.getElementById('date');
+   var hasil6 = document.getElementById('TL');              
+   hasil6.innerHTML = input.value;
+   var input = document.getElementById('jenkel');
+   var hasil7 = document.getElementById('jk');              
+   hasil7.innerHTML = input.value;
+   var input = document.getElementById('Organisasi');
+   var hasil8 = document.getElementById('o');              
+   hasil8.innerHTML = input.value;
+    var input = document.getElementById('misi');
+   var hasil9 = document.getElementById('mo');              
+   hasil9.innerHTML = input.value;
+   var input = document.getElementById('visi');
+   var hasi20 = document.getElementById('vm');              
+   hasi20.innerHTML = input.value;
+   
+} 
